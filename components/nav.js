4b80b7a6ad5b784be4c87eed7770361b989ee648
Konrad Sobolewski <konradsob12@gmail.com>
2019-10-12 01:37:16 +0200

    navigation

--- components/nav.js
+++ components/nav.js
@@ -7,33 +7,34 @@ import '../styles/navigation.module.scss'
 import NavigationContext from "./NavigationContext";
 
 const Nav = (props) => {
-    const [ route, setRoute ] = useContext(NavigationContext);
+    const [route, setRoute] = useContext(NavigationContext);
     return (
         <Headroom style={{backgroundColor: 'white'}}>
             <Link href={"/"}>
-                <img src={logoImage} id='logoImage' className={'logo'} alt={"Logo of Jacht-Plast"} onClick={() => setRoute('/')}/>
+                <img src={logoImage} id='logoImage' className={'logo'} alt={"Logo of Jacht-Plast"}
+                     onClick={() => setRoute('/')}/>
             </Link>
             <ul className={'list'}>
-                <li onClick={() => setRoute('/')} className={route === '/' ? 'pressed' : ''}>
-                    <Link href={"/"}>
-                        <a>O nas</a>
-                    </Link>
-                </li>
-                <li onClick={() => setRoute('/gallery')} className={route === '/gallery' ? 'pressed' : ''}>
-                    <Link href={"/gallery"}>
-                        <a>Galeria</a>
-                    </Link>
-                </li>
-                <li  onClick={() => setRoute('/transport')} className={route === '/transport' ? 'pressed' : ''}>
-                    <Link href={"/transport"} >
-                        <a>Transport</a>
-                    </Link>
-                </li>
-                <li  onClick={() => setRoute('/contact')} className={route === '/contact' ? 'pressed' : ''}>
-                    <Link href={"/contact"}>
-                        <a>Kontakt</a>
-                    </Link>
-                </li>
+                <Link href={"/"}>
+                    <li onClick={() => setRoute('/')} className={route === '/' ? 'pressed' : ''}>
+                        O nas
+                    </li>
+                </Link>
+                <Link href={"/gallery"}>
+                    <li onClick={() => setRoute('/gallery')} className={route === '/gallery' ? 'pressed' : ''}>
+                        Galeria
+                    </li>
+                </Link>
+                <Link href={"/transport"}>
+                    <li onClick={() => setRoute('/transport')} className={route === '/transport' ? 'pressed' : ''}>
+                        Transport
+                    </li>
+                </Link>
+                <Link href={"/contact"}>
+                    <li onClick={() => setRoute('/contact')} className={route === '/contact' ? 'pressed' : ''}>
+                        Kontakt
+                    </li>
+                </Link>
             </ul>
         </Headroom>
     )
