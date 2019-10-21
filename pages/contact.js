dfd88674859e8b4244018fbd2d62961c86092a48
PrzemekSobolewski <przemus212@gmail.com>
2019-10-21 21:16:31 +0200

    added: email form, email form style

--- pages/contact.js
+++ pages/contact.js
@@ -2,27 +2,36 @@ import React from 'react'
 import Head from 'next/head'
 import Nav from '../components/nav'
 import Layout from "../components/Layout";
+import ReactContactForm from 'react-mail-form';
 
 const Contact = () => (
     <Layout>
         <div className={"contact_div"} style={{height: 1000, backgroundColor: 'gray'}}>
-            <div style={{padding:4}}>
+            <div className={"contact_data"} style={{padding:4}}>
                 <h1>Jacht-Plast</h1>
                 <h2>Zdzisław Sobolewski</h2>
 
-                <h3>Adres</h3>
+                <h3>Adres:</h3>
                 Ul. Przemysłowa 4<br/>
                 26-340 Drzewica<br/>
-                <h3>Kontakt</h3>
+                <h3>Kontakt:</h3>
                 Tel/fax (48) 375 67 35<br/>
                 Tel. kom. 601 256 133<br/>
                 <h3>Email:</h3>
                 jachtplast@gmail.com<br/>
-                biuro@budowairemontyjachtow.pl<br/>
             </div>
-            <div className="google_maps">
-                <iframe
-    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9945.949450330274!2d20.4618076!3d51.4492078!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x72536c3f299847fc!2sPHUP%20JACHT-PLAST!5e0!3m2!1spl!2spl!4v1571140334742!5m2!1spl!2spl"/>
+            <div className={"mail_map_div"}>
+                <div className={"mail_form"}>
+                    <input placeholder={"Email"} type={"text"} name={"email"} className={""}/>
+                    <input placeholder={"Temat"} type={"text"} name={"topic"} className={""}/>
+                    <textarea placeholder={"Wiadomość"} className={"message"}>
+
+                    </textarea>
+                </div>
+                <div className="google_maps">
+                    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9945.949450330274!2d20.4618076!3d51.4492078!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x72536c3f299847fc!2sPHUP%20JACHT-PLAST!5e0!3m2!1spl!2spl!4v1571140334742!5m2!1spl!2spl"/>
+                </div>
             </div>
         </div>
     </Layout>
--- pages/production.js
+++ pages/production.js
@@ -1,13 +1,73 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Head from 'next/head'
 import Nav from '../components/nav'
 import Layout from "../components/Layout";
+import Expand from "react-expand-animated"
+import dreamer from "../assets/images/production/dreamer.jpg"
 
-const Production = () => (
-    <Layout>
-        <div style={{height: 1000, backgroundColor: 'gray'}}>
-            Test
-        </div>
-    </Layout>
-);
+const elements = [
+    {
+        id: 1,
+        open: false,
+        photo: dreamer,
+        title: 'Tes 32 Dreamer',
+        desc: 'text 1'
+    },
+    {
+        id: 2,
+        open: false,
+        photo: dreamer,
+        title: 'Tes 32 Dreamer',
+        desc: 'text 2'
+    }
+];
+const Production = () => {
+    const [data, setData] = useState(elements);
+
+
+    const openExpand = (element) => {
+        setData(data.map(i => {
+                if(element.id === i.id && i.open === true) {
+                    i.open = false;
+                    return i;
+                }
+                i.open = element.id === i.id;
+                return i;
+            })
+        );
+    };
+
+    let list = data.map(i => {
+        return (
+            <div onClick={() => openExpand(i)} className={'expandButton'}>
+                {i.title}
+            </div>
+        )
+    });
+
+    let content = data.map(i => {
+        return (
+            <Expand open={i.open}
+                    duration={1000}>
+                <div style={{display: "flex"}}>
+                    <img src={i.photo}/>
+                    <div>{i.desc}</div>
+                </div>
+            </Expand>
+        )
+    });
+    return (
+        <Layout>
+            <div className={"production"} style={{height: 1000}}>
+                <div className={"list_div"}>
+                    {list}
+                </div>
+
+                <div className={"content_div"}>
+                    {content}
+                </div>
+            </div>
+        </Layout>
+    )
+};
 export default Production;
