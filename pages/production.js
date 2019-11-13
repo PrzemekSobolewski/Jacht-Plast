cef8c816919bede722c9e7d0c1bc2d5bcfe242c1
PrzemekSobolewski <przemus212@gmail.com>
2019-11-13 20:51:13 +0100

    added:  production

--- pages/production.js
+++ pages/production.js
@@ -3,27 +3,83 @@ import Head from 'next/head'
 import Nav from '../components/nav'
 import Layout from "../components/Layout";
 import Expand from "react-expand-animated"
-import dreamer from "../assets/images/production/dreamer.jpg"
+import Carousel, {Modal, ModalGateway} from 'react-images'
+
+
+function importAll(r) {
+    return r.keys().map(r);
+}
+
+const bt_tes = importAll(require.context('../assets/images/production/bt_tes/', false, /\.(png|jpe?g|svg)$/));
+const dreamer = importAll(require.context('../assets/images/production/dreamer/', false, /\.(png|jpe?g|svg)$/));
+const dreamer_m = importAll(require.context('../assets/images/production/dreamer_m/', false, /\.(png|jpe?g|svg)$/));
+const illuminatus = importAll(require.context('../assets/images/production/illuminatis/', false, /\.(png|jpe?g|svg)$/));
+const master = importAll(require.context('../assets/images/production/master/', false, /\.(png|jpe?g|svg)$/));
+const magnam = importAll(require.context('../assets/images/production/magnam/', false, /\.(png|jpe?g|svg)$/));
+const versus = importAll(require.context('../assets/images/production/versus/', false, /\.(png|jpe?g|svg)$/));
 
 const elements = [
     {
         id: 1,
         open: false,
-        photo: dreamer,
-        title: 'Tes 32 Dreamer',
+        photo: master,
+        title: 'TES 550 MASTER',
         desc: 'text 1'
     },
     {
         id: 2,
         open: false,
+        photo: bt_tes,
+        title: 'TES 678 BT-TES 720',
+        desc: 'text 1'
+    },
+    {
+        id: 3,
+        open: false,
+        photo: versus,
+        title: 'TES 246 VERSUS',
+        desc: 'text 1'
+    },
+    {
+        id: 4,
+        open: false,
+        photo: magnam,
+        title: 'TES 28 MAGNAM',
+        desc: 'text 1'
+    },
+    {
+        id: 5,
+        open: false,
         photo: dreamer,
-        title: 'Tes 32 Dreamer',
+        title: 'TES 32 DREAMER',
+        desc: 'text 1'
+    },
+    {
+        id: 6,
+        open: false,
+        photo: dreamer_m,
+        title: 'TES 32 DREAMER MOTOROWY',
+        desc: 'text 2'
+    },
+    {
+        id: 7,
+        open: false,
+        photo: illuminatus,
+        title: 'TES 393 ILLUMINATUS',
         desc: 'text 2'
     }
+
 ];
 const Production = () => {
     const [data, setData] = useState(elements);
+    const [lightboxIsOpen, setlightboxIsOpen] = useState(false);
+    const [number, setNumber] = useState(0);
 
+    const openLightBox = (j) => {
+        console.log(number);
+        setNumber(j);
+        setlightboxIsOpen(true)
+    };
 
     const openExpand = (element) => {
         setData(data.map(i => {
@@ -39,27 +95,62 @@ const Production = () => {
 
     let list = data.map(i => {
         return (
-            <div onClick={() => openExpand(i)} className={i.open ? 'expandButton openedExpand' : 'expandButton'} style={{display: 'block'}}  >
-                <img src={i.photo} width={'200px'} height={'200px'}/>
-                {i.title}
+            <div className={'fullExpand'}>
+                <div onClick={() => openExpand(i)} className={i.open ? 'expandButton openedExpand' : 'expandButton'} style={{display: 'block'}}>
+                    {i.title}
+                </div>
                 <Expand open={i.open}
                         className={'expand'}
                         duration={1000}>
-                    <div>{i.desc}</div>
+                        <div>{i.desc}</div>
+                    {i.photo.map(j =>{
+                        return (
+                            <Images key={j}>
+                                <img className={'productionImage'} src={j} width={'200px'} height={'200px'} onClick={() => openLightBox(j)}/>
+                            </Images>
+                        )
+                    })}
+                    <ModalGateway>
+                        {lightboxIsOpen ? (
+                            <Modal onClose={() => setlightboxIsOpen(false)}>
+                                <Carousel
+                                    currentIndex={number}
+                                    views={i.photo}
+                                    frameProps = {{
+                                        autoSize : 'height'
+                                    }}
+                                    autoSize={ {
+                                        width: 2000,
+                                        height: 500
+                                    }}
+                                />
+                            </Modal>
+                        ) : null}
+                    </ModalGateway>
+
                 </Expand>
+
             </div>
         )
     });
 
     return (
         <Layout>
-            <div className={"production"} style={{height: 1000}}>
-                <div className={"list_div"} style={{display: 'flex'}}>
+            <div className={"production"}>
+                <div className={"list_div"}>
                     {list}
                 </div>
 
             </div>
         </Layout>
     )
+};
+
+const Images = (props) => {
+    return (
+        <div>
+            {props.children}
+        </div>
+    );
 };
 export default Production;
