54689296f0170b0211ceb44ed293de8fd7da2f3b
Konrad Sobolewski <konradsob12@gmail.com>
2019-11-17 22:16:03 +0100

    added style to transport and about us

--- pages/transport.js
+++ pages/transport.js
@@ -6,7 +6,9 @@ import Layout from "../components/Layout";
 import Carousel, {Modal, ModalGateway} from 'react-images'
 import Images from '../components/Images'
 import Gallery from '../components/Gallery'
-import { IoIosArrowDropdown } from "react-icons/io";
+import {IoIosArrowDropdown, IoIosCheckmark} from "react-icons/io";
+import {Star} from '@material-ui/icons'
+import Fade from 'react-reveal/Fade'
 
 function importAll(r) {
     return r.keys().map(r);
@@ -37,20 +39,28 @@ const Transport = () => {
                 <img className={"image"} src={i} onClick={() => openLightBox(j)}/>
             </Images>)
     };
-    return(
+    return (
         <Layout>
             <div className={"transportText"}>
-            <div style={{textAlign:'center'}}><h2>P.H.U.P.,,JACHT-PLAST"</h2></div>
-                <ul>
-                    <li>Świadczymy usługi z zakresu krajowego i międzynarodowego transportu towarów,</li>
-                    <li>Zapewniamy w pełni profesjonalny i ubezpieczony transport jachtów żaglowych i łodzi motorowych oraz pojazdów,</li>
-                    <li>Dysponujemy samochodami dostosowanymi do przewozu jednostek do 12 m długości i masie 8 ton oraz mniejszych, poniżej 3,5 tony,</li>
-                    <li>Posiadamy wymagane w transporcie drogowym zezwolenia i ubezpieczenia ładunku,</li>
-                    <li>Ustalamy indywidualne ceny, w zależności od zakresu uaługi oraz odpowiadamy na zadawane mailem lub telefonicznie pytania.</li>
-                </ul>
-                <div style={{textAlign:'center'}}><h3>Nasza firma posiada wieloletnie doświadczenie w budowie i naprawie jachtów żaglowych i motorowych, co daje nam wiedzę o potrzebach klientów zainteresowanych transportem swoich jednostek.<br/>
-                <br/>
-                Kliknij poniżej żeby zobaczyć galerię!</h3></div>
+                <div style={{textAlign: 'center'}}><h2>P.H.U.P. "JACHT-PLAST"</h2></div>
+                <Fade> <ul className={'transportList'}>
+                    <li><IoIosCheckmark className={'checkmark'}/>Świadczymy usługi z zakresu krajowego i międzynarodowego transportu towarów</li>
+                    <li><IoIosCheckmark className={'checkmark'}/>Zapewniamy w pełni profesjonalny i ubezpieczony transport jachtów żaglowych i łodzi motorowych
+                        oraz pojazdów
+                    </li>
+                    <li><IoIosCheckmark className={'checkmark'}/>Dysponujemy samochodami dostosowanymi do przewozu jednostek do 12 m długości i masie 8 ton oraz
+                        mniejszych, poniżej 3,5 tony
+                    </li>
+                    <li><IoIosCheckmark className={'checkmark'}/>Posiadamy wymagane w transporcie drogowym zezwolenia i ubezpieczenia ładunku</li>
+                    <li><IoIosCheckmark className={'checkmark'}/>Ustalamy indywidualne ceny, w zależności od zakresu uaługi oraz odpowiadamy na zadawane mailem
+                        lub telefonicznie pytania
+                    </li>
+                </ul></Fade>
+                <div className={'underList'}><h3>Nasza firma posiada wieloletnie doświadczenie w budowie i
+                    naprawie jachtów żaglowych i motorowych, co daje nam wiedzę o potrzebach klientów zainteresowanych
+                    transportem swoich jednostek.<br/>
+                    <br/>
+                    Kliknij poniżej żeby zobaczyć galerię!</h3></div>
 
             </div>
             <div className={"transportLine"}>
@@ -72,10 +82,10 @@ const Transport = () => {
                                 <Carousel
                                     currentIndex={number}
                                     views={readyImages}
-                                    frameProps = {{
-                                        autoSize : 'height'
+                                    frameProps={{
+                                        autoSize: 'height'
                                     }}
-                                    autoSize={ {
+                                    autoSize={{
                                         width: 2000,
                                         height: 500
                                     }}
