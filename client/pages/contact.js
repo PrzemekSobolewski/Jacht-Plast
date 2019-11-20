47142832d78c43b2585bf6854bc2ce2ead08265a
Konrad Sobolewski <konradsob12@gmail.com>
2019-11-20 23:37:27 +0100

    contact form style

--- client/pages/contact.js
+++ client/pages/contact.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import Layout from "../components/Layout";
+import Layout from '../components/Layout';
 import axios from 'axios';
 
 const Contact = () => {
@@ -11,7 +11,7 @@ const Contact = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         const instance = axios.create({
-            baseURL: "http://localhost:8080",
+            baseURL: 'http://localhost:8080',
         });
         if (!((name.trim().length || email.trim().length || subject.trim().length) === 0)) {
             let response = await instance.post('/api/form', {
@@ -21,13 +21,13 @@ const Contact = () => {
                 message: message
             });
             if (response.data.msg === 'success') {
-                alert("Message Sent.");
+                alert('Message Sent.');
                 resetForm();
             } else if (response.data.msg === 'fail') {
-                alert("Message failed to send.")
+                alert('Message failed to send.')
             }
         } else {
-            alert("Przed wysłaniem wiadomości wypełnij wszystkie pola");
+            alert('Przed wysłaniem wiadomości wypełnij wszystkie pola');
         }
     };
 
@@ -40,9 +40,9 @@ const Contact = () => {
 
     return (
         <Layout>
-            <div className={"contact_div"}>
-                <div className={"mail_data_div"}>
-                    <div className={"contact_data"}>
+            <div className={'contact_div'}>
+                <div className={'mail_data_div'}>
+                    <div className={'contact_data'}>
                         <h1>Jacht-Plast</h1>
                         <h2>Zdzisław Sobolewski</h2>
 
@@ -55,24 +55,24 @@ const Contact = () => {
                         <h3>Email:</h3>
                         jachtplast@gmail.com<br/>
                     </div>
-                    <div className={"mail_form"}>
-                        <form id="contact-form" method="POST" role={'form'} onSubmit={handleSubmit}>
-                            <input placeholder={"Imię i nazwisko"} type={"text"} name={"name"} id={'name'}
-                                   className={""} value={name} onChange={e => setName(e.target.value)}/>
-                            <input placeholder={"Email"} type={"text"} name={"email"} id={'email'} className={""}
+                    <div className={'mail_form'}>
+                        <form id='contact-form' className={'contact-form'} method='POST' role={'form'} onSubmit={handleSubmit}>
+                            <input placeholder={'Imię i nazwisko'} type={'text'} name={'name'} id={'name'}
+                                   value={name} onChange={e => setName(e.target.value)}/>
+                            <input placeholder={'Email'} type={'text'} name={'email'} id={'email'}
                                    value={email} onChange={e => setEmail(e.target.value)}/>
-                            <input placeholder={"Temat"} type={"text"} name={"subject"} id={'subject'} className={""}
+                            <input placeholder={'Temat'} type={'text'} name={'subject'} id={'subject'}
                                    value={subject} onChange={e => setSubject(e.target.value)}/>
-                            <textarea className={"message"} id={'message'} name={"message"} value={message}
+                            <textarea id={'message'} name={'message'} value={message} placeholder={'Wpisz wiadomość...'}
                                       onChange={e => setMessage(e.target.value)}/>
-                            <button type="submit" className="submitButton">Wyślij</button>
+                            <button type='submit' className='submitButton'>Wyślij</button>
                         </form>
                     </div>
                 </div>
-                <div className={"map_div"}>
-                    <div className="google_maps">
+                <div className={'map_div'}>
+                    <div className='google_maps'>
                         <iframe
-                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9945.949450330274!2d20.4618076!3d51.4492078!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x72536c3f299847fc!2sPHUP%20JACHT-PLAST!5e0!3m2!1spl!2spl!4v1571140334742!5m2!1spl!2spl"/>
+                            src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9945.949450330274!2d20.4618076!3d51.4492078!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x72536c3f299847fc!2sPHUP%20JACHT-PLAST!5e0!3m2!1spl!2spl!4v1571140334742!5m2!1spl!2spl'/>
                     </div>
                 </div>
             </div>
