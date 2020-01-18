2c5ecc72848c0b4adaed35e23b746c299e4f5971
PrzemekSobolewski <przemus212@gmail.com>
2020-01-18 19:40:51 +0100

    text to index

--- client/pages/index.js
+++ client/pages/index.js
@@ -26,12 +26,12 @@ const Home = () => (
         <div className={"about_us_div"}>
             <div className={'about_us_first'}>
                 <div>
-                    Firma "JACHT – PLAST” zajmuje się budową jachtów żaglowych i motorowych. Od wielu lat jesteśmy
-                    głównym
-                    wykonawcą marki ,,TES” . Jachty przez nas wykonane charakteryzują się ciekawą zabudową
-                    wnętrza, zapewniają komfort i wygodę na miarę potrzeb każdego klienta. Nasze jednostki spełniają
-                    wymagane
-                    standardy jakości i bezpieczeństwa.
+                    Firma "JACHT – PLAST” zajmuje się budową jachtów żaglowych i motorowych.
+                    Jachty przez nas wykonane charakteryzują się ciekawą zabudową
+                    wnętrza, zapewniają komfort i wygodę. Do każdego projektu podchodzimy indywidualnie,
+                    tworząc dzięki temu jednostki będące dumą naszą jak i ich właścicieli.
+                    Możemy się poszczycić blisko 20-letnim doświadczeniem w branży. Daje nam to wiedzę, którą chętnie dzielimy się z klientem.
+                    Nasze łodzie spełniają wymagane standardy jakości i bezpieczeństwa.
                 </div>
                 <img src={logoImage} className={'about_us_logo'} alt={"Logo of Jacht-Plast"}/>
             </div>
@@ -41,16 +41,16 @@ const Home = () => (
                     Posiadamy również wieloletnie doświadczenie w produkcji laminatów poliestrowo – szklanych. Nasze wyroby
                     produkowane są z wysokiej jakości materiałów, co zapewnia im trwałość w użytkowaniu. Współpracujemy z wieloma dostawcami,
                     zapewniając naszym klientom wybór urządzeń wyposażenia jachtu.
-                    Dzięki wieloletniemu doświadczeniu w produkcji laminatów, realizujemy również zlecenia na dowolny rodzaj
-                    wyrobów laminatowych. Duże możliwości
-                    produkcyjne oraz wieloletnie doświadczenie zawodowe pracowników, dają gwarancję solidnego wykonania
+                    Dzięki zdobytemu doświadczeniu w produkcji laminatów, realizujemy również zlecenia na dowolny rodzaj
+                    wyrobów laminatowych.
+                    Duże możliwości produkcyjne oraz niezaprzeczalna kompetentność pracowników, dają gwarancję solidnego wykonania
                     wyrobu.
                 </div>
             </div>
             <div className={'about_us_first'}>
                 <div>
-                    Zapewniamy serwis wykonanych łodzi oraz świadczymy usługi napraw i remontów używanego sprzętu
-                    pływającego.
+                    Nie tylko zajmujemy się produkcją, ale również zapewniamy serwis wykonanych łodzi.
+                    Dodatkowo świadczymy usługi napraw i remontów używanego sprzętu pływającego.
                 </div>
                 <img src={logoImage} className={'about_us_photo'} alt={"Logo of Jacht-Plast"}/>
             </div>
