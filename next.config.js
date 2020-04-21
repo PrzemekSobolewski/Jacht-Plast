11e644727c76933fa264aaad92b91b6cfd1e68ab
PrzemekSobolewski <przemus212@gmail.com>
2020-04-21 13:00:31 +0200

    jpg loader

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withImages = require('next-images');
 const withFonts = require('next-fonts');
 const withSass = require('@zeit/next-sass');
 
-module.exports =
+module.exports = 
     withImages(withSass(withFonts({
             enableSvg: true,
             poweredByHeader: false,
@@ -21,3 +21,28 @@ module.exports =
             }
         },
     )));
+
+module.exports = {
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    presets: ['react', 'es2015', 'stage-2']
+                }
+            },
+        ],
+        rules: [
+            {
+              test: /\.(png|jpe?g|gif)$/i,
+              use: [
+                {
+                  loader: 'file-loader',
+                },
+              ],
+            },
+        ],
+    }
+}
