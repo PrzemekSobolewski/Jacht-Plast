d8ea01823609fef04fafc410f4f6fe80c90e2e53
PrzemekSobolewski <przemus212@gmail.com>
2020-04-23 22:21:52 +0200

    config

--- next.config.js
+++ next.config.js
@@ -1,26 +1,39 @@
 const withImages = require('next-images');
 const withFonts = require('next-fonts');
 const withSass = require('@zeit/next-sass');
-const optimizedImages = require('next-optimized-images')
+const optimizedImages = require('next-optimized-images');
+const path = require('path');
+const withPlugins = require('next-compose-plugins');
 
-module.exports = 
-    withImages(withSass(withFonts(optimizedImages({
-            enableSvg: true,
-            poweredByHeader: false,
-            webpack(config, options) {
-                config.resolve.alias['components'] = path.join(__dirname, 'components')
-                config.resolve.alias['static'] = path.join(__dirname, 'static')
-                return config;
-            },
-            exportPathMap: async function (
-                defaultPathMap, {dev, dir, outDir, distDir, buildId}
-            ) {
-                return {
-                    '/': {page: '/'},
-                    '/production.html': {page: '/production'},
-                    '/transport.html': {page: '/transport'},
-                    '/contact.html': {page: '/contact'},
-                }
-            }
-        },
-    ))));
+const nextConfig = {
+    enableSvg: true,
+    poweredByHeader: false,
+    webpack(config, options) {
+        config.resolve.alias['components'] = path.join(__dirname, 'components')
+        config.resolve.alias['static'] = path.join(__dirname, 'static')
+        return config;
+    },
+    exportPathMap: async function (
+        defaultPathMap, {dev, dir, outDir, distDir, buildId}
+    ) {
+        return {
+            '/': {page: '/'},
+            '/production.html': {page: '/production'},
+            '/transport.html': {page: '/transport'},
+            '/contact.html': {page: '/contact'},
+        }
+    }
+}
+
+module.exports = withPlugins(
+    [
+        [optimizedImages, { handleImages: ['jpeg', 'png']}],
+        withFonts,
+        withSass,
+        withImages
+    ],
+    nextConfig
+);
+
+
+ 
