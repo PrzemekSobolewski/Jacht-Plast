3fdc556aae5d2f56469bf26e8ad1f6ce91e83865
PrzemekSobolewski <przemus212@gmail.com>
2020-07-12 10:49:31 +0200

    images update

--- components/JachtSlider.js
+++ components/JachtSlider.js
@@ -2,24 +2,27 @@ import React from 'react'
 import {ScrollTo} from "react-scroll-to";
 import {useSelector} from "react-redux";
 import water from "../assets/images/slides/water.jpg";
-import truck from "../assets/images/slides/truck.jpg";
 import exterior from "../assets/images/slides/exterior.jpg";
-
+import boat from "../assets/images/slides/boat.jpg";
+import interior from "../assets/images/slides/interior.jpg";
 const JachtSlider = () => {
     const switchState = useSelector(state => state.switch);
     const images = [
         water,
-        truck,    
-        exterior,   
+        interior,
+        exterior,
+        boat
     ];
     
     return (
         <div>
             <div className={'bgContainer2'}>
                 <div className="fadein">
-                    <img id="f3" src={images[0]}/>
-                    <img id="f2" src={images[1]}/>
-                    <img id="f1" src={images[2]}/>
+                    <img id="f1" src={images[1]}/>
+                    <img id="f2" src={images[0]}/>
+                    <img id="f3" src={images[2]}/>
+                    <img id="f4" src={images[3]}/>
+
                 </div>
                 <ScrollTo>
                     {({scrollTo}) => 
